65b4c22ae62c2eb832af9a334c65ad7a6dd16e96
julia <juliiagordin@gmail.com>
2024-04-18 13:22:36 +0300

    lesson 3

--- src/test/java/tests/StartProjectTest.java
+++ src/test/java/tests/StartProjectTest.java
@@ -1,14 +1,36 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartProjectTest {
 
     @Test
     public void openGooglePage() {
-        WebDriver driver = new ChromeDriver();
+        // connection thought folder Tools and installed drivers for Chrome and FireFox
+        // WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
+
+        // Connection with WebDriverManager
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--lang=en");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        //----------------------------------------
+//        FirefoxOptions firefoxOptions = new FirefoxOptions();
+//        firefoxOptions.addPreference("intl.accept_languages", "en");
+//        WebDriverManager.firefoxdriver().setup();
+//        WebDriver driver = new FirefoxDriver(firefoxOptions);
+        //-------------------------
+
         driver.navigate().to("https://www.google.com/");
         System.out.println("we open google");
 
@@ -17,6 +39,26 @@ public class StartProjectTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        //WebElement element = driver.findElement(By.xpath("//img[@alt='Google']")); -> only first element
+
+        List<WebElement> elements = driver.findElements(By.xpath("//img[@alt='Google']"));
+
+        //-----
+//        driver.navigate().refresh();
+//        driver.navigate().forward();
+//        driver.navigate().back();
+
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+
+        //----
+
+        // all 3 asserts check the same
+//        Assert.assertTrue(elements.size() > 0);
+//        Assert.assertTrue(!elements.isEmpty());
+        Assert.assertFalse(elements.isEmpty());
+
         driver.quit();
     }
 }
